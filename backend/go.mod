ae69b7b6b1472fcfd8745f31ddf4325f87001d1f
Kasper Ingelvide <kasper@hubben.xyz>
2021-02-14 14:51:58 +0100

    Create resource endpoint

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module bultdatabasen
 go 1.15
 
 require (
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	gorm.io/driver/mysql v1.0.4
 	gorm.io/gorm v1.20.12
--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,13 @@ import (
 	authorizer "bultdatabasen/middleware"
 	"bultdatabasen/model"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"encoding/json"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -61,9 +63,40 @@ func getResource(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(resource)
 }
 
+func createChildResource(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+    parentResourceID := vars["resourceID"]
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var resource model.Resource 
+    json.Unmarshal(reqBody, &resource)
+
+	var depth int32
+	switch resource.Type {
+	case "area":
+		depth = 100
+	case "crag":
+		depth = 200
+	case "sector":
+		depth = 300
+	case "route":
+		depth = 400
+	case "installation":
+		depth = 500
+	}
+	
+	resource.ID = uuid.Must(uuid.NewRandom()).String()
+	resource.ParentID = &parentResourceID
+	resource.Depth = depth
+	model.DB.Create(&resource)
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(resource)
+}
+
 func checkHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
-	
+
     w.WriteHeader(http.StatusOK)
     io.WriteString(w, `{"alive": true}`)
 }
@@ -77,9 +110,12 @@ func main() {
 	router.Use(mux.CORSMethodMiddleware(router))
 
 	router.HandleFunc("/resources/{resourceID}", getResource).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/resources/{resourceID}", createChildResource).Methods(http.MethodPost, http.MethodOptions)
 	router.HandleFunc("/resources/{resourceID}/ancestors", getResourceAncestors).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/resources/{resourceID}/sectors", getSectors).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/resources/{resourceID}/routes", getRoutes).Methods(http.MethodGet, http.MethodOptions)
+
+
 
 	router.HandleFunc("/health", checkHandler)
 
