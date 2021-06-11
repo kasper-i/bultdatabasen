cc7cb0e549bef926a14cb0209e5189de245428e3
Kasper Ingelvide <kasper@hubben.xyz>
2021-06-11 21:46:31 +0200

    Reverse list of ancestors

--- backend/api/ancestors.go
+++ backend/api/ancestors.go
@@ -15,6 +15,10 @@ func GetAncestors(w http.ResponseWriter, r *http.Request) {
 	if ancestors, err := model.GetAncestors(model.DB, id); err != nil {
 		utils.WriteError(w, err)
 	} else {
+		for i, j := 0, len(ancestors)-1; i < j; i, j = i+1, j-1 {
+			ancestors[i], ancestors[j] = ancestors[j], ancestors[i]
+		}
+		
 		utils.WriteResponse(w, http.StatusOK, ancestors)
 	}
 }
