7e21e9373021a384410033fc04a29e9f9fdbaec8
Kasper Ingelvide <kasper@hubben.xyz>
2022-03-08 17:35:03 +0100

    Add test data disclaimer

--- web/src/layouts/Main.tsx
+++ web/src/layouts/Main.tsx
@@ -5,6 +5,9 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div className="w-screen min-h-screen flex flex-col bg-gray-100">
+      <div className="bg-red-500 h-8 flex justify-center items-center text-white">
+        <div>Allt innehåll på sidan består av testdata!</div>
+      </div>
       <NavigationBar />
       <div className="relative flex-grow">
         <div className="mx-auto p-5" style={{ maxWidth: 768 }}>
