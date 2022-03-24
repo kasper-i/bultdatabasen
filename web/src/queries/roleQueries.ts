9960a775a5767c49df8b3075b4dd954c9e4e98dd
Kasper Ingelvide <kasper@hubben.xyz>
2022-03-24 13:07:15 +0100

    Make useRole more stable

--- web/src/queries/roleQueries.ts
+++ web/src/queries/roleQueries.ts
@@ -1,15 +1,29 @@
 import { Api } from "@/Api";
+import { selectAuthenticated } from "@/slices/authSlice";
+import { useAppSelector } from "@/store";
+import { AxiosError } from "axios";
 import { useQuery } from "react-query";
 
 export const useRole = (resourceId: string) => {
+  const isAuthenticated = useAppSelector(selectAuthenticated);
+
   const { data } = useQuery(
     ["role", { resourceId }],
     async () => Api.getUserRoleForResource(resourceId),
     {
       select: (role) => role.role,
-      staleTime: 1000 * 60 * 30,
+      retry: (failureCount, error: AxiosError) => {
+        if (error.response?.status === 401 || error.response?.status === 403) {
+          return false;
+        }
+
+        return failureCount <= 2;
+      },
+      enabled: isAuthenticated,
+      staleTime: 1000 * 60 * 15,
+      cacheTime: 1000 * 60 * 15,
     }
   );
 
-  return { role: data };
+  return { role: data ?? "guest" };
 };
