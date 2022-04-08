a9a22b2ddec08960e878c8968fcba30604072492
Kasper Ingelvide <kasper@hubben.xyz>
2022-04-08 22:29:33 +0200

    Use Link

--- web/src/components/ChildrenTable.tsx
+++ web/src/components/ChildrenTable.tsx
@@ -2,7 +2,7 @@ import { ResourceType } from "@/models/resource";
 import { useChildren } from "@/queries/resourceQueries";
 import { getResourceLabel, getResourceRoute } from "@/utils/resourceUtils";
 import React, { Fragment, ReactElement } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface Props {
   resourceId: string;
@@ -11,7 +11,6 @@ interface Props {
 
 const ChildrenTable = ({ resourceId, filters }: Props): ReactElement => {
   const children = useChildren(resourceId);
-  const navigate = useNavigate();
 
   if (children.data == null) {
     return <Fragment />;
@@ -30,17 +29,15 @@ const ChildrenTable = ({ resourceId, filters }: Props): ReactElement => {
             const label = getResourceLabel(resource.type);
             const url = getResourceRoute(resource.type, resource.id);
             return (
-              <li
-                key={resource.id}
-                className="cursor-pointer"
-                onClick={() => url && navigate(url)}
-              >
-                <div className="flex justify-start items-center my-1.5 gap-2.5">
-                  <span className="text-xl">{resource.name}</span>
-                  <span className="bg-primary-400 rounded-full py-1 px-2 text-xs text-white">
-                    {label}
-                  </span>
-                </div>
+              <li key={resource.id}>
+                <Link to={url}>
+                  <div className="flex justify-start items-center my-1.5 gap-2.5">
+                    <span className="text-xl">{resource.name}</span>
+                    <span className="bg-primary-400 rounded-full py-1 px-2 text-xs text-white">
+                      {label}
+                    </span>
+                  </div>
+                </Link>
               </li>
             );
           })}
