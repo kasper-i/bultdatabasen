6c1f1477716c734bd5ba8bd63f4f8cb9a2513bbc
Kasper Ingelvide <kasper@hubben.xyz>
2022-04-27 22:04:06 +0200

    Remove warning banner

--- web/src/layouts/Main.tsx
+++ web/src/layouts/Main.tsx
@@ -5,9 +5,6 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div className="w-screen min-h-screen flex flex-col">
-      <div className="bg-red-500 h-8 flex justify-center items-center text-white">
-        <div>Allt innehåll på sidan består av testdata!</div>
-      </div>
       <NavigationBar />
       <div className="relative flex flex-grow">
         <div className="flex-grow mx-auto p-5" style={{ maxWidth: 768 }}>
