0b2bb1c7363f84f4f91e228977f2c4a1831477cd
Kasper Ingelvide <github@exapoint.net>
2022-05-09 21:07:25 +0200

    Make task delete confirmed (#109)

--- web/src/components/features/task/TaskButton.tsx
+++ web/src/components/features/task/TaskButton.tsx
@@ -8,13 +8,18 @@ interface Props {
 }
 
 function TaskButton({ resourceId }: Props): ReactElement {
-  const tasks = useTasks(resourceId);
+  const { data: tasks } = useTasks(resourceId);
+
+  const taskCount =
+    tasks?.filter(
+      (task) => task.status === "open" || task.status === "assigned"
+    )?.length ?? 0;
 
   return (
     <div className="w-min relative cursor-pointer">
       <IconButton circular icon="wrench" />
-      {tasks.data && tasks.data.length > 0 && (
-        <Pill className="absolute -top-2.5 -right-2">{tasks.data.length}</Pill>
+      {taskCount > 0 && (
+        <Pill className="absolute -top-2.5 -right-2">{taskCount}</Pill>
       )}
     </div>
   );
