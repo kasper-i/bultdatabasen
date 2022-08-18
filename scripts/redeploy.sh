fbf0fa2bebb870fa9c65e1b37bbde7beb10595d4
Kasper Ingelvide <kasper@hubben.xyz>
2022-08-18 19:54:50 +0200

    Remove token

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 ARTIFACT_URL=$1
 ARCHIVE_URL=$(curl -s $ARTIFACT_URL | jq -r '.artifacts[0].archive_download_url')
-URL=$(curl -sI -H "Authorization: token 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" $ARCHIVE_URL | awk 'BEGIN {FS=": "}/^location/{print $2}')
+URL=$(curl -sI -H "Authorization: token <TOKEN>" $ARCHIVE_URL | awk 'BEGIN {FS=": "}/^location/{print $2}')
 wget "$URL" -qO /tmp/artifact.zip
 DEB=$(zipinfo -1 /tmp/artifact.zip)
 unzip -oq /tmp/artifact.zip -d /tmp
