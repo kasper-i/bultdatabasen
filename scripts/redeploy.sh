593926c5afd78e0d79b7c3b6629bab3de5e4b51c
Kasper Ingelvide <github@exapoint.net>
2022-08-22 20:07:12 +0200

    Update deploy script (#165)

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -e
+
+# Access tokens require repo scope for private repositories and public_repo scope for public repositories.
+ACCESS_TOKEN="ghp_"
+
 ARTIFACT_URL=$1
 ARCHIVE_URL=$(curl -s $ARTIFACT_URL | jq -r '.artifacts[0].archive_download_url')
-URL=$(curl -sI -H "Authorization: token <TOKEN>" $ARCHIVE_URL | awk 'BEGIN {FS=": "}/^location/{print $2}')
+URL=$(curl -sI -H "Authorization: token $ACCESS_TOKEN" $ARCHIVE_URL | awk 'BEGIN {FS=": "}/^location/{print $2}')
 wget "$URL" -qO /tmp/artifact.zip
 DEB=$(zipinfo -1 /tmp/artifact.zip)
 unzip -oq /tmp/artifact.zip -d /tmp
-dpkg -i --force-confdef --force-confold "/tmp/$DEB"
+apt install "/tmp/$DEB"
 rm /tmp/artifact.zip "/tmp/$DEB"
 systemctl daemon-reload
 systemctl restart bultdatabasen
