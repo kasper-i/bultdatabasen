6c59a860e4bbc64ac4f4ae94eb85c462ca0713c5
Kasper Ingelvide <github@exapoint.net>
2022-08-22 19:39:38 +0200

    Add scripts (#164)

---
+++ scripts/redeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+ARTIFACT_URL=$1
+ARCHIVE_URL=$(curl -s $ARTIFACT_URL | jq -r '.artifacts[0].archive_download_url')
+URL=$(curl -sI -H "Authorization: token <TOKEN>" $ARCHIVE_URL | awk 'BEGIN {FS=": "}/^location/{print $2}')
+wget "$URL" -qO /tmp/artifact.zip
+DEB=$(zipinfo -1 /tmp/artifact.zip)
+unzip -oq /tmp/artifact.zip -d /tmp
+dpkg -i --force-confdef --force-confold "/tmp/$DEB"
+rm /tmp/artifact.zip "/tmp/$DEB"
+systemctl daemon-reload
+systemctl restart bultdatabasen
