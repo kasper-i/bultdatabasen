7f7bd1901f53b14c27a3f3ab3e893affad53571c
Kasper Ingelvide <kasper@hubben.xyz>
2022-09-25 14:07:31 +0200

    Fix typo

--- backend/api/tasks.go
+++ backend/api/tasks.go
@@ -32,9 +32,9 @@ func GetTasks(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	statues := query["status"]
+	statuses := query["status"]
 
-	if tasks, meta, err := sess.GetTasks(parentResourceId, pagination, statues); err != nil {
+	if tasks, meta, err := sess.GetTasks(parentResourceId, pagination, statuses); err != nil {
 		utils.WriteError(w, err)
 	} else {
 		response := GetTasksResponse{}
