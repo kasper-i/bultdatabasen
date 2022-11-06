ba6c92e4c691e312f324771b60ac9ccc2f8a73d8
Kasper Ingelvide <kasper@hubben.xyz>
2022-11-06 20:12:49 +0100

    No anchor number on routes with only a single anchor

--- web/src/components/features/routeEditor/hooks.ts
+++ web/src/components/features/routeEditor/hooks.ts
@@ -1,22 +1,30 @@
 import { Point } from "@/models/point";
 import { useMemo } from "react";
 
-export type PointLabel = { name: string; no: number };
+export type PointLabel = { name: string; no?: string };
 
 export const usePointLabeler = (points: Point[]) => {
   return useMemo(() => {
     const labels: Map<string, PointLabel> = new Map();
 
-    let anchors = 1;
-    let nonAnchors = 1;
+    let anchorNo = 1;
+    let nonAnchorNo = 1;
+
+    const multiAnchorRoute = points.filter((point) => point.anchor).length > 1;
 
     for (const point of points) {
+      let no: number | undefined = point.anchor ? anchorNo++ : nonAnchorNo++;
+      if (point.anchor && !multiAnchorRoute) {
+        no = undefined;
+      }
+
       labels.set(point.id, {
         name: point.anchor ? "Ankare" : "Punkt",
-        no: point.anchor ? anchors++ : nonAnchors++,
+        no: no ? `№${no}` : undefined,
       });
     }
 
-    return (pointId: string) => labels.get(pointId) ?? { name: "", no: 0 };
+    return (pointId: string): PointLabel =>
+      labels.get(pointId) ?? { name: "?", no: "" };
   }, [points]);
 };
