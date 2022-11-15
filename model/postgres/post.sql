7da0384f11f85ea6aa1f4bdbc6ea4bee8c5df43c
Kasper Ingelvide <kasper@hubben.xyz>
2022-11-15 19:34:09 +0100

    Add SQL procedure to populate tree

--- model/postgres/post.sql
+++ model/postgres/post.sql
@@ -13,12 +13,43 @@ CREATE INDEX path_gist_idx ON tree USING GIST (path);
 CREATE INDEX path_idx ON tree USING BTREE (path);
 
 CREATE OR REPLACE FUNCTION
-  populate_path(ID varchar)
+  populate_path(RESOURCE_ID varchar)
 RETURNS
-  void
+  ltree
 AS $$
 DECLARE
+    f RECORD;
+    path varchar;
 
 BEGIN
-  RAISE NOTICE 'A simple text';
+    FOR f IN SELECT id, parent_id FROM resource WHERE id = RESOURCE_ID
+        UNION
+            SELECT foster_care.id, foster_parent_id AS parent_id
+            FROM foster_care
+            WHERE foster_care.id = RESOURCE_ID
+    LOOP
+        SELECT string_agg(id, '.') INTO path FROM (
+            SELECT * FROM (
+                WITH RECURSIVE cte (id, parent_id, c, dummy) AS (
+            		SELECT id, parent_id, 1, TRUE
+            		FROM resource
+            		WHERE id = f.parent_id
+            	UNION DISTINCT
+            		SELECT parent.id, parent.parent_id, cte.c + 1, TRUE
+            		FROM resource parent
+            		INNER JOIN cte ON parent.id=cte.parent_id
+            	)
+            	SELECT REPLACE(id, '-', '_') AS id FROM cte
+                ORDER BY c DESC
+            ) i
+            UNION ALL SELECT REPLACE(f.parent_id, '-', '_')
+            UNION ALL SELECT REPLACE(f.id, '-', '_')
+        ) o;
+    END LOOP;
+
+    RETURN path::ltree;
 END $$ LANGUAGE plpgsql;
+
+INSERT INTO tree SELECT id, populate_path(id) AS path FROM resource WHERE type <> 'root' AND depth < 600 AND parent_id IS NOT NULL;
+
+
