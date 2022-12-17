d8d2ea8efa703ecc8c8d3ff83e85d51d313668ab
Kasper Ingelvide <kasper@hubben.xyz>
2022-12-17 14:34:48 +0100

    Make user pool configurable in app

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,4 +1,1 @@
 export const rootNodeId = "7ea1df97-df3a-436b-b1d2-b211f1b9b363";
-export const cognitoUrl =
-  "https://bultdatabasen.auth.eu-west-1.amazoncognito.com";
-export const cognitoClientId = "4bc4eb6q54d9poodouksahhk86";
