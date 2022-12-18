edb63c1a8c4094faf4b0bc705cae2e2b4b238671
Kasper Ingelvide <kasper@hubben.xyz>
2022-12-18 19:06:49 +0100

    Fix build error

--- web/src/components/LoginToolbar.tsx
+++ web/src/components/LoginToolbar.tsx
@@ -2,19 +2,11 @@ import configData from "@/config.json";
 import { selectAuthenticated } from "@/slices/authSlice";
 import { useAppSelector } from "@/store";
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 import Button from "./atoms/Button";
 
 function LoginToolbar(): ReactElement {
   const isAuthenticated = useAppSelector(selectAuthenticated);
-
-  const gotoCognitoSignin = () => {
-    localStorage.setItem("returnPath", window.location.pathname);
-
-    const callback =
-      window.location.protocol + "//" + window.location.host + "/signin";
-
-    window.location.href = `${configData.COGNITO_URL}/login?client_id=${configData.COGNITO_CLIENT_ID}&response_type=code&scope=profile+openid&redirect_uri=${callback}`;
-  };
 
   const gotoCognitoSignout = () => {
     localStorage.setItem("returnPath", window.location.pathname);
@@ -38,14 +30,11 @@ function LoginToolbar(): ReactElement {
     );
   } else {
     return (
-      <Button
-        outlined
-        color="white"
-        onClick={gotoCognitoSignin}
-        className="ring-offset-primary-300"
-      >
-        Logga In
-      </Button>
+      <Link to="/signin">
+        <Button outlined color="white" className="ring-offset-primary-300">
+          Logga In
+        </Button>
+      </Link>
     );
   }
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -21,6 +21,5 @@ export default defineConfig({
   },
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-    global: {},
   },
 });
