7420d4333fca3f3a7668a21ca5969b179fac1351
Kasper Ingelvide <kasper@hubben.xyz>
2022-12-20 21:16:29 +0100

    More work on register page

--- web/src/pages/RegisterPage.tsx
+++ web/src/pages/RegisterPage.tsx
@@ -1,16 +1,27 @@
+import { Api } from "@/Api";
 import { Alert } from "@/components/atoms/Alert";
 import Button from "@/components/atoms/Button";
 import Input from "@/components/atoms/Input";
+import { login } from "@/slices/authSlice";
+import { useAppDispatch } from "@/store";
 import {
   confirmRegistration,
+  signin,
   signUp,
   translateCognitoError,
 } from "@/utils/cognito";
-import { CognitoUserAttribute } from "amazon-cognito-identity-js";
+import {
+  AuthenticationDetails,
+  CognitoUserAttribute,
+} from "amazon-cognito-identity-js";
 
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   const [phase, setPhase] = useState<1 | 2>(1);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -21,7 +32,9 @@ const RegisterPage = () => {
   const [errorMessage, setErrorMessage] = useState<string>();
 
   const register = async () => {
+    setInProgress(true);
     setErrorMessage(undefined);
+
     const attributeList: CognitoUserAttribute[] = [];
 
     attributeList.push(
@@ -46,17 +59,46 @@ const RegisterPage = () => {
       setPhase(2);
     } catch (err) {
       setErrorMessage(translateCognitoError(err));
+    } finally {
+      setInProgress(false);
     }
   };
 
-  const confirm = () => {
+  const confirm = async () => {
+    setInProgress(true);
+
     try {
       confirmRegistration(email, confirmationCode);
+
+      const authenticationDetails = new AuthenticationDetails({
+        Username: email,
+        Password: password,
+      });
+
+      const result = await signin(authenticationDetails);
+      const accessToken = result.getAccessToken().getJwtToken();
+      const idToken = result.getIdToken().getJwtToken();
+      const refreshToken = result.getRefreshToken().getToken();
+
+      Api.setTokens(idToken, accessToken, refreshToken);
+
+      await Api.updateMyself({ email: email, firstName: givenName, lastName });
+
+      const returnPath = localStorage.getItem("returnPath");
+      localStorage.removeItem("returnPath");
+
+      dispatch(login({ firstName: givenName, lastName }));
+
+      navigate(returnPath != null ? returnPath : "/");
     } catch (err) {
       setErrorMessage(JSON.stringify(err));
       return;
+    } finally {
+      setInProgress(false);
     }
   };
+
+  const canRegister = !!email && !!password && !!givenName && !!lastName;
 
   return (
     <div className="flex flex-col items-center gap-2.5">
@@ -89,6 +131,7 @@ const RegisterPage = () => {
               tabIndex={4}
             />
           </div>
+
           <hr />
 
           <Alert>{errorMessage}</Alert>
@@ -97,6 +140,7 @@ const RegisterPage = () => {
             loading={inProgress}
             full
             onClick={register}
+            disabled={!canRegister}
           >
             Registrera
           </Button>
@@ -108,12 +152,10 @@ const RegisterPage = () => {
             value={confirmationCode}
             onChange={(e) => setConfirmationCode(e.target.value)}
           />
-          <Input
-            label="Lösenord"
-            value={password}
-            password
-            onChange={(e) => setPassword(e.target.value)}
-          />
+
+          <hr />
+
+          <Alert>{errorMessage}</Alert>
           <Button
             className="mt-2.5"
             loading={inProgress}
