7f076c5c1345456913adb17f97e8b28bcc21ed11
Kasper Ingelvide <kasper@hubben.xyz>
2022-12-22 16:03:09 +0100

    Add missing module

---
+++ web/src/components/atoms/constants.ts
@@ -0,0 +1,37 @@
+export enum Color {
+  Danger = "#EF4444",
+  White = "white",
+  Primary = "#7D2AE8",
+  Disabled = "#9ca3af",
+}
+
+export enum ExtendedColor {
+  Label = "#374151",
+}
+
+export enum Size {
+  Base = "2rem",
+}
+
+export enum Shadow {
+  Sm = "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+}
+
+export enum Rounding {
+  Base = "0.25rem",
+}
+
+export enum FontSize {
+  Xs = "0.75rem",
+  Sm = "0.875rem",
+  Base = "1rem",
+  Lg = "1.125rem",
+}
+
+export enum FontWeight {
+  Md = "500",
+}
+
+export enum Border {
+  Thin = "1px",
+}
