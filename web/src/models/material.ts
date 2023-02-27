ba7d1546ac4c730feed862c495e4ff8443fa50f9
Kasper Ingelvide <kasper@hubben.xyz>
2023-02-27 19:52:58 +0100

    Use string().uuid()

--- web/src/models/material.ts
+++ web/src/models/material.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const materialSchema = z.object({
-  id: z.string(),
-  name: z.string(),
+  id: z.string().uuid(),
+  name: z.string().min(1),
 });
 
 export type Material = z.infer<typeof materialSchema>;
