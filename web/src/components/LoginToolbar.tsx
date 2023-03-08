64199090aae4e51234a8cc58dabc3bbd510a3725
Kasper Ingelvide <kasper@hubben.xyz>
2023-03-08 20:43:29 +0100

    Remove state

--- web/src/components/LoginToolbar.tsx
+++ web/src/components/LoginToolbar.tsx
@@ -17,7 +17,7 @@ function LoginToolbar(): ReactElement {
     );
   } else {
     return (
-      <Link to="/auth/signin" state={{ returnPath: window.location.pathname }}>
+      <Link to="/auth/signin">
         <Button outlined color="white" className="ring-offset-primary-300">
           Logga In
         </Button>
