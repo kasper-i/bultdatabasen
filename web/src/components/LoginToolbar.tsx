b881e36676c7ce39b4e03cfeb7c74876fee2a6ce
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2023-03-08 21:02:16 +0100

    Improve sign in experience (#287)

--- web/src/components/LoginToolbar.tsx
+++ web/src/components/LoginToolbar.tsx
@@ -1,4 +1,3 @@
-import configData from "@/config.json";
 import { selectAuthenticated } from "@/slices/authSlice";
 import { useAppSelector } from "@/store";
 import { ReactElement } from "react";
@@ -8,25 +7,13 @@ import Button from "./atoms/Button";
 function LoginToolbar(): ReactElement {
   const isAuthenticated = useAppSelector(selectAuthenticated);
 
-  const gotoCognitoSignout = () => {
-    localStorage.setItem("returnPath", window.location.pathname);
-
-    const callback =
-      window.location.protocol + "//" + window.location.host + "/signout";
-
-    window.location.href = `${configData.COGNITO_URL}/logout?client_id=${configData.COGNITO_CLIENT_ID}&logout_uri=${callback}`;
-  };
-
   if (isAuthenticated) {
     return (
-      <Button
-        outlined
-        color="white"
-        onClick={gotoCognitoSignout}
-        className="ring-offset-primary-300"
-      >
-        Logga Ut
-      </Button>
+      <Link to="/auth/signout">
+        <Button outlined color="white" className="ring-offset-primary-300">
+          Logga Ut
+        </Button>
+      </Link>
     );
   } else {
     return (
