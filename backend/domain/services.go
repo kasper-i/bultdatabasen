695bbec1ad5317294f7bbff8f9137faeef29f546
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2023-05-08 17:46:15 +0200

    Update backend/domain/services.go

--- backend/domain/services.go
+++ backend/domain/services.go
@@ -3,5 +3,5 @@ package domain
 import "context"
 
 type EmailSender interface {
-	SendEmail(ctx context.Context, recipient string, subject string, body string) error
+	SendEmail(ctx context.Context, recipient, subject, body string) error
 }
