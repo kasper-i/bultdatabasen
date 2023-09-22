a8d7b27c8d17cb4e84176e572da1781897830f37
Kasper Ingelvide <kasper@hubben.xyz>
2023-09-22 19:12:52 +0200

    Replace Dialog and Transition

--- web/src/components/ImageCarousel.tsx
+++ web/src/components/ImageCarousel.tsx
@@ -1,8 +1,7 @@
 import configData from "@/config.json";
 import { Image, ImageRotation, ImageVersion } from "@/models/image";
 import { useDeleteImage, useUpdateImage } from "@/queries/imageQueries";
-import { Dialog, Transition } from "@headlessui/react";
-import { ActionIcon, Button, Loader } from "@mantine/core";
+import { ActionIcon, Button, Loader, Modal, Transition } from "@mantine/core";
 import { IconDownload, IconRotateClockwise2, IconX } from "@tabler/icons-react";
 import clsx from "clsx";
 import {
@@ -93,83 +92,75 @@ export const FullSizeImage: FC<{
   }
 
   return (
-    <Transition appear show as={Fragment}>
-      <Dialog className="fixed inset-0 z-10 overflow-y-auto" onClose={onClose}>
-        <div className="min-h-screen flex flex-col justify-center items-center overflow-hidden">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            entered="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-neutral-50" />
-          </Transition.Child>
-
-          {loading && (
-            <div className="fixed flex items-center justify-center -mt-16">
-              <Loader type="bars" />
-            </div>
-          )}
-
-          <div className="-mt-16" tabIndex={1}>
-            <img
-              className={clsx(loading && "invisible")}
-              ref={imgRef}
-              onLoad={onLoad}
-              style={{
-                imageOrientation: "none",
-                ...dimensionClasses,
-                ...rotatorClasses,
-              }}
-              src={`${configData.API_URL}/images/${image.id}/${version}`}
-              alt=""
-            />
+    <Modal
+      opened
+      onClose={close}
+      fullScreen
+      radius={0}
+      transitionProps={{ transition: "fade", duration: 200 }}
+      withCloseButton={false}
+    >
+      <div className="min-h-screen flex flex-col justify-center items-center overflow-hidden">
+        {loading && (
+          <div className="fixed flex items-center justify-center -mt-16">
+            <Loader type="bars" />
           </div>
+        )}
 
-          <div className="fixed h-16 w-full bottom-0 inset-x-0 flex justify-between px-5 bg-neutral-100">
-            <div className="flex items-center gap-2.5">
-              <ActionIcon onClick={onClose} variant="subtle">
-                <IconX size={14} />
-              </ActionIcon>
-              <Restricted>
-                <Button
-                  loading={updateImage.isLoading}
-                  onClick={() =>
-                    updateImage.mutate({
-                      rotation: image.rotation
-                        ? (((image.rotation + 90) % 360) as ImageRotation)
-                        : 90,
-                    })
-                  }
-                  leftSection={<IconRotateClockwise2 size={14} />}
-                  className="ring-offset-neutral-100"
-                >
-                  Rotera
-                </Button>
-              </Restricted>
+        <div className="-mt-16" tabIndex={1}>
+          <img
+            className={clsx(loading && "invisible")}
+            ref={imgRef}
+            onLoad={onLoad}
+            style={{
+              imageOrientation: "none",
+              ...dimensionClasses,
+              ...rotatorClasses,
+            }}
+            src={`${configData.API_URL}/images/${image.id}/${version}`}
+            alt=""
+          />
+        </div>
 
+        <div className="fixed h-16 w-full bottom-0 inset-x-0 flex justify-between px-5 bg-neutral-100">
+          <div className="flex items-center gap-2.5">
+            <ActionIcon onClick={onClose} variant="subtle">
+              <IconX size={14} />
+            </ActionIcon>
+            <Restricted>
               <Button
                 loading={updateImage.isLoading}
                 onClick={() =>
-                  (window.location.href = `${configData.API_URL}/images/${image.id}`)
+                  updateImage.mutate({
+                    rotation: image.rotation
+                      ? (((image.rotation + 90) % 360) as ImageRotation)
+                      : 90,
+                  })
                 }
-                leftSection={<IconDownload size={14} />}
+                leftSection={<IconRotateClockwise2 size={14} />}
                 className="ring-offset-neutral-100"
               >
-                Original
+                Rotera
               </Button>
-              <Restricted>
-                <ConfirmedDeleteButton target="bilden" mutation={deleteImage} />
-              </Restricted>
-            </div>
+            </Restricted>
+
+            <Button
+              loading={updateImage.isLoading}
+              onClick={() =>
+                (window.location.href = `${configData.API_URL}/images/${image.id}`)
+              }
+              leftSection={<IconDownload size={14} />}
+              className="ring-offset-neutral-100"
+            >
+              Original
+            </Button>
+            <Restricted>
+              <ConfirmedDeleteButton target="bilden" mutation={deleteImage} />
+            </Restricted>
           </div>
         </div>
-      </Dialog>
-    </Transition>
+      </div>
+    </Modal>
   );
 };
 
