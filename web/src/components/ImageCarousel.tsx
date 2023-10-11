fe4f3cf0b04f4ec170a93a52a6f342bb8121c0a6
Kasper Ingelvide <kasper@hubben.xyz>
2023-10-11 19:50:51 +0200

    ImageCarousel

--- web/src/components/ImageCarousel.tsx
+++ web/src/components/ImageCarousel.tsx
@@ -1,9 +1,8 @@
 import configData from "@/config.json";
 import { Image, ImageRotation, ImageVersion } from "@/models/image";
 import { useDeleteImage, useUpdateImage } from "@/queries/imageQueries";
-import { ActionIcon, Button, Loader, Modal, Transition } from "@mantine/core";
+import { ActionIcon, Button, Group, Loader } from "@mantine/core";
 import { IconDownload, IconRotateClockwise2, IconX } from "@tabler/icons-react";
-import clsx from "clsx";
 import {
   CSSProperties,
   FC,
@@ -13,8 +12,9 @@ import {
   useRef,
   useState,
 } from "react";
-import ConfirmedDeleteButton from "./molecules/ConfirmedDeleteButton";
+import classes from "./ImageCarousel.module.css";
 import Restricted from "./Restricted";
+import ConfirmedDeleteButton from "./molecules/ConfirmedDeleteButton";
 
 type Orientation = "portrait" | "landscape";
 
@@ -77,8 +77,8 @@ export const FullSizeImage: FC<{
   };
 
   let dimensionClasses: CSSProperties = {
-    maxHeight: "calc(100vh - 4rem)",
-    maxWidth: "calc(100vw)",
+    maxHeight: "round(up, calc(100vh - 4rem), 1px)",
+    maxWidth: "round(up, calc(100vw), 1px)",
   };
 
   if (targetOrientation !== originalOrientation) {
@@ -92,75 +92,60 @@ export const FullSizeImage: FC<{
   }
 
   return (
-    <Modal
-      opened
-      onClose={close}
-      fullScreen
-      radius={0}
-      transitionProps={{ transition: "fade", duration: 200 }}
-      withCloseButton={false}
-    >
-      <div data-tailwind="min-h-screen flex flex-col justify-center items-center overflow-hidden">
-        {loading && (
-          <div data-tailwind="fixed flex items-center justify-center -mt-16">
-            <Loader type="bars" />
-          </div>
-        )}
-
-        <div data-tailwind="-mt-16" tabIndex={1}>
-          <img
-            data-tailwind={clsx(loading && "invisible")}
-            ref={imgRef}
-            onLoad={onLoad}
-            style={{
-              imageOrientation: "none",
-              ...dimensionClasses,
-              ...rotatorClasses,
-            }}
-            src={`${configData.API_URL}/images/${image.id}/${version}`}
-            alt=""
-          />
+    <div className={classes.modal}>
+      {loading && (
+        <div className={classes.loaderContainer}>
+          <Loader type="bars" />
         </div>
-
-        <div data-tailwind="fixed h-16 w-full bottom-0 inset-x-0 flex justify-between px-5 bg-neutral-100">
-          <div data-tailwind="flex items-center gap-2.5">
-            <ActionIcon onClick={onClose} variant="subtle">
-              <IconX size={14} />
-            </ActionIcon>
-            <Restricted>
-              <Button
-                loading={updateImage.isLoading}
-                onClick={() =>
-                  updateImage.mutate({
-                    rotation: image.rotation
-                      ? (((image.rotation + 90) % 360) as ImageRotation)
-                      : 90,
-                  })
-                }
-                leftSection={<IconRotateClockwise2 size={14} />}
-                data-tailwind="ring-offset-neutral-100"
-              >
-                Rotera
-              </Button>
-            </Restricted>
-
-            <Button
-              loading={updateImage.isLoading}
-              onClick={() =>
-                (window.location.href = `${configData.API_URL}/images/${image.id}`)
-              }
-              leftSection={<IconDownload size={14} />}
-              data-tailwind="ring-offset-neutral-100"
-            >
-              Original
-            </Button>
-            <Restricted>
-              <ConfirmedDeleteButton target="bilden" mutation={deleteImage} />
-            </Restricted>
-          </div>
-        </div>
+      )}
+      <div className={classes.imgContainer} tabIndex={1}>
+        <img
+          ref={imgRef}
+          onLoad={onLoad}
+          style={{
+            visibility: loading ? "hidden" : "visible",
+            ...dimensionClasses,
+            ...rotatorClasses,
+          }}
+          src={`${configData.API_URL}/images/${image.id}/${version}`}
+          alt=""
+        />
       </div>
-    </Modal>
+      <Group gap="sm" justify="space-between" className={classes.toolbar}>
+        <ActionIcon onClick={onClose} variant="subtle">
+          <IconX size={14} />
+        </ActionIcon>
+        <div className={classes.filler} />
+        <Restricted>
+          <Button
+            loading={updateImage.isLoading}
+            onClick={() =>
+              updateImage.mutate({
+                rotation: image.rotation
+                  ? (((image.rotation + 90) % 360) as ImageRotation)
+                  : 90,
+              })
+            }
+            leftSection={<IconRotateClockwise2 size={14} />}
+          >
+            Rotera
+          </Button>
+        </Restricted>
+
+        <Button
+          loading={updateImage.isLoading}
+          onClick={() =>
+            (window.location.href = `${configData.API_URL}/images/${image.id}`)
+          }
+          leftSection={<IconDownload size={14} />}
+        >
+          Original
+        </Button>
+        <Restricted>
+          <ConfirmedDeleteButton target="bilden" mutation={deleteImage} />
+        </Restricted>
+      </Group>
+    </div>
   );
 };
 
