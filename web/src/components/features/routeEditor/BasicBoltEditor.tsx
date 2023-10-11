2d5201da3378034cc966c6e9f3c0580f90ed4846
Kasper Ingelvide <kasper@hubben.xyz>
2023-10-11 20:10:23 +0200

    BasicBoltEditor

--- web/src/components/features/routeEditor/BasicBoltEditor.tsx
+++ web/src/components/features/routeEditor/BasicBoltEditor.tsx
@@ -1,8 +1,8 @@
 import { Option } from "@/components/atoms/types";
 import { Bolt, BoltType } from "@/models/bolt";
 import { positionToLabel, translateBoltType } from "@/utils/boltUtils";
-import { ActionIcon, Group, Radio } from "@mantine/core";
-import { IconTrack, IconTrash } from "@tabler/icons-react";
+import { ActionIcon, Card, Group, Radio, Text } from "@mantine/core";
+import { IconTrash } from "@tabler/icons-react";
 
 const typeOptions = (["expansion", "glue", "piton"] as const).map<
   Option<BoltType>
@@ -30,19 +30,19 @@ const BasicBoltEditor = ({
   };
 
   return (
-    <div data-tailwind="w-28 border-2 border-primary-500 rounded-md flex flex-col justify-between p-2">
-      <div data-tailwind="flex justify-between items-center mb-2">
-        <p data-tailwind="text-left font-medium">
+    <Card withBorder>
+      <Group justify="space-between" align="center">
+        <Text fw={500}>
           {positionToLabel(
             totalNumberOfBolts === 1 ? undefined : bolt.position
           )}
-        </p>
+        </Text>
         {onRemove && (
           <ActionIcon onClick={onRemove} variant="subtle" color="red">
             <IconTrash size={14} />
           </ActionIcon>
         )}
-      </div>
+      </Group>
 
       <Radio.Group
         label="Typ"
@@ -59,7 +59,7 @@ const BasicBoltEditor = ({
           ))}
         </Group>
       </Radio.Group>
-    </div>
+    </Card>
   );
 };
 
